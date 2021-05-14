27321568d7705af50fb798a6cc42d56f4c21f651
Patricia Huancaya <patricia_huancaya@glic.com>
2021-05-14 12:32:43 -0400

    User model in place

---
+++ models/users.js
@@ -0,0 +1,12 @@
+const users = [
+    {
+        username: "CRHarding", 
+        email: "c@c.com", 
+        password: "casey123",
+        profile: "Lorem ipsum dolor sit amet dolor lorem sit amet.",
+        img: "https://st3.depositphotos.com/15648834/17930/v/1600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg",
+        uuid: 12345
+    }
+]
+
+module.exports = users;
--- server.js
+++ server.js
@@ -4,3 +4,4 @@ const app = express();
 app.listen(3000, (req, res) => {
     console.log("Up and listening on port 3000!");
 })
+
